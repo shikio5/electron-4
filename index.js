8c997e0b977011e0c206b889629c94a559929dde
Danny Wu <shikio@gmail.com>
2021-12-31 00:38:51 -0500

    Show and hide automatically the about window

--- index.js
+++ index.js
@@ -2,19 +2,41 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindows() {
     let appWindow = new BrowserWindow({
-        width:600,
-        height:800,
-        center:true,
+        width: 600,
+        height: 800,
+        center: true,
         minWidth: 300
     });
     appWindow.loadFile('./index.html');
 
-    appWindow.on('closed', ()=> {
+    appWindow.on('closed', () => {
         appWindow = null
     });
 
+    let aboutWindow = new BrowserWindow({
+        width: 300,
+        height: 275,
+        frame: false,
+        transparent: true
+    });
+
+    aboutWindow.loadFile('./about.html');
+
+    aboutWindow.on('closed', () => {
+        aboutWindow = null
+    });
+
     appWindow.once('ready-to-show', () => {
+        appWindow.maximize();
         appWindow.show();
+
+        setTimeout(() => {
+            aboutWindow.show();
+            setTimeout(() => {
+                aboutWindow.hide();
+            }, 5000);
+        }, 1000);
+
     })
 }
 
